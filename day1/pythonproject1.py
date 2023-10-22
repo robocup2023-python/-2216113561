ec5ca74530566535b89a2ae05c04ac9897f03122
Williamdu29 <146198051+Williamdu29@users.noreply.github.com>
2023-10-22 20:45:46 +0800

    day1

---
+++ day1/pythonproject1.py
@@ -0,0 +1,42 @@
+#1
+
+# count=0
+# for i in range(1,5):
+#     for j in range(1,5):
+#         for k in range(1,5):
+#             for m in range(1,5):
+#                 if i==j or i==k or i==m or k==m or j==m:
+#                     continue
+#                 count+=1
+#                 print(i,j,k,m)
+# print("---------")
+# print(count)
+
+
+
+#2
+
+# a=[12,124,45]
+# a.sort()
+# print(a)
+
+
+#3
+
+# f1=0
+# f2=1
+# print(f1)
+# print(f2)
+# for i in range(18):
+#     f3=f2+f1
+#     print(f3)
+#     f1=f2
+#     f2=f3
+
+#4
+
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         print(f"{i}*{j}={i*j}\t",end=' ')
+#         if j==i:
+#             print("\n")
